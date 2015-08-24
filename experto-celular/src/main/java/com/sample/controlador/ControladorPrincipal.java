b85218dda8be72f384f50134cb1c47f90f564b6a
beimarhuarachi <beimar020@gmail.com>
2015-08-23 22:45:45 -0400

    agregado libreria para estilos de vista y modificado las clases de vista

--- experto-celular/src/main/java/com/sample/controlador/ControladorPrincipal.java
+++ experto-celular/src/main/java/com/sample/controlador/ControladorPrincipal.java
@@ -1,15 +1,39 @@
 package com.sample.controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import com.sample.modelo.Diagnostico;
 import com.sample.modelo.Problema;
 import com.sample.modelo.TipoEstado;
+import com.sample.vista.PanelProblemaAudio;
+import com.sample.vista.VistaPrincipal;
 
-public class ControladorPrincipal {
+public class ControladorPrincipal implements ActionListener{
 
-	public static void main(String[] args) {
+	VistaPrincipal vistaPrincipal;
+	
+	public ControladorPrincipal(VistaPrincipal vistaPrincipal) {
+		this.vistaPrincipal = vistaPrincipal;
+		this.vistaPrincipal.agregarEvento(this);
+	}
+	
+	
+
+	public void actionPerformed(ActionEvent arg0) {
 		
-		Problema e  = new Problema(TipoEstado.AUDIO);
-		
+		if(arg0.getSource() == vistaPrincipal.btnAceptar) {
+			String problema = (String) vistaPrincipal.comboBox.getSelectedItem();
+			
+			/**
+			 * Controla que se cambie el panel de problema de Audio
+			 */
+			if (problema == "Problema con el Audio") {
+				vistaPrincipal.cambiarPanel("Audio");
+			}
+			
+			
+		}
 		
 	}
 }
--- experto-celular/src/main/java/com/sample/vista/VistaPrincipal.java
+++ experto-celular/src/main/java/com/sample/vista/VistaPrincipal.java
@@ -1,12 +1,19 @@
 package com.sample.vista;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.EventQueue;
+import java.awt.MenuBar;
+
+import javafx.scene.image.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JMenuBar;
@@ -22,97 +29,117 @@ import java.awt.event.ActionListener;
 public class VistaPrincipal extends JFrame {
 
 	private JPanel contentPane;
-	private JPanel panelCuestionarioParaCambiar;
+	public JPanel panelParaCambiar;
+	public JButton btnAceptar;
+	public CardLayout cardLayout;
+	public JComboBox comboBox;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VistaPrincipal frame = new VistaPrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public VistaPrincipal() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 905, 524);
+		this.configurarVista();
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 633, 21);
-		contentPane.add(menuBar);
+		this.iniciarMenu();
+		this.agregarImagen();
 		
+
+		this.iniciarPanelParaCambiar();
+		
+		this.iniciarPanelCentral();
+		
+
+		
+	}
+	
+	public void cambiarPanel(String panel) {
+		this.cardLayout.show(this.panelParaCambiar, panel);
+	}
+	
+	public void iniciarPanelParaCambiar() {
+		panelParaCambiar = new JPanel();
+		this.cardLayout = new CardLayout();
+		panelParaCambiar.setLayout(this.cardLayout);
+		panelParaCambiar.setBorder(BorderFactory.createTitledBorder("Panel de Cambio"));
+		panelParaCambiar.setBounds(448, 12, 430, 440);
+		
+		/**
+		 * Aqui se agrega los paneles que cambian 
+		 */
+		panelParaCambiar.add(new JPanel(), "Inicio");
+		panelParaCambiar.add(new PanelProblemaAudio(), "Audio");
+		
+		contentPane.add(panelParaCambiar);
+	}
+	
+	
+
+	public void iniciarPanelCentral() {
+		JPanel panel = new JPanel();
+		panel.setBounds(26, 293, 384, 173);
+		contentPane.add(panel);
+
+		JLabel lblNewLabel1 = new JLabel("Indique su Problema: ");
+		lblNewLabel1.setBounds(28, 78, 200, 50);
+		panel.add(lblNewLabel1);
+
+		comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {
+				"Problema de Carga", "Problema con el Audio",
+				"Problema con el Display", "Problemas de Apagado" }));
+		comboBox.setBounds(164, 92, 181, 22);
+		panel.add(comboBox);
+
+		btnAceptar = new JButton("Aceptar");
+		btnAceptar.setBounds(164, 141, 97, 25);
+
+		panel.add(btnAceptar);
+	}
+	
+	public void iniciarMenu() {
+		JMenuBar menuBar = new JMenuBar();
+
+		this.setJMenuBar(menuBar);
+
 		JMenu mnArchivo = new JMenu("Archivo");
 		menuBar.add(mnArchivo);
-		
+
 		JMenuItem mntmCerrar = new JMenuItem("Cerrar");
 		mnArchivo.add(mntmCerrar);
-		
+
 		JMenu mnAbout = new JMenu("About");
 		menuBar.add(mnAbout);
-		
+
 		JMenuItem mntmDocumento = new JMenuItem("Documento");
 		mnAbout.add(mntmDocumento);
-		
-		JLabel lblImagen = new JLabel("Imagen");
-		lblImagen.setBackground(Color.MAGENTA);
-		lblImagen.setBounds(30, 88, 380, 159);
-		contentPane.add(lblImagen);
-		
-		panelCuestionarioParaCambiar = new JPanel();
-		panelCuestionarioParaCambiar.setBounds(448, 61, 398, 413);
-		contentPane.add(panelCuestionarioParaCambiar);
-		
+	}
+
+	public void agregarImagen() {
 		JLabel lblNewLabel = new JLabel("Diagnostico para Celulares");
 		lblNewLabel.setBounds(26, 35, 367, 42);
 		contentPane.add(lblNewLabel);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(26, 293, 384, 173);
-		contentPane.add(panel);
-		
-		JLabel lblNewLabel1 = new JLabel("Indique su Problema: ");
-		lblNewLabel1.setBounds(28, 78, 200, 50);
-		panel.add(lblNewLabel1);	
-		
-		final JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Problema de Carga", "Problema con el Audio", "Problema con el Display", "Problemas de Apagado"}));
-		comboBox.setBounds(164, 92, 181, 22);
-		panel.add(comboBox);
-		final VistaPrincipal a = this;
-		JButton btnAceptar = new JButton("Aceptar");
-		btnAceptar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String problema = (String)comboBox.getSelectedItem();
-				if(problema == "Problema de Carga"){
-					//carga.setVisible(true);
-					a.setPanelCuestionario(new PanelProblemaAudio());
-				}
-			}
-		});
-		btnAceptar.setBounds(164, 141, 97, 25);
-		
-		panel.add(btnAceptar);
+		JLabel lblImagen = new JLabel();
+		lblImagen.setIcon(new ImageIcon("img/descarga.jpg"));
+		lblImagen.setBackground(Color.MAGENTA);
+		lblImagen.setBounds(20, 68, 480, 220);
+		contentPane.add(lblImagen);
 	}
 	
-	public void setPanelCuestionario(PanelProblemaAudio panelNuevo) {
-		this.removeAll();
-		this.add(panelNuevo);
-		this.repaint();
-		
-		SwingUtilities.updateComponentTreeUI(this);
+	public void agregarEvento(ActionListener action) {
+		this.btnAceptar.addActionListener(action);
+	}
+
+	public void configurarVista() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 905, 524);
+		this.setResizable(false);
+		this.setTitle("Sistemas Expertos-Diagnostico Celulares");
+	}
+
+	public void iniciar() {
+		this.setVisible(true);
 	}
 }
